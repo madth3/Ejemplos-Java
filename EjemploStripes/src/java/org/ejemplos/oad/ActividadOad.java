5e6b48312a43af2ad3b0132e4a01435fdcfef8d3
Julio Aguilar <madth3.dev@gmail.com>
2011-09-19 00:45:21 -0500

    Clases que simulan el acceso a datos para cargar los objetos del modelo.

---
+++ EjemploStripes/src/java/org/ejemplos/oad/ActividadOad.java
@@ -0,0 +1,29 @@
+package org.ejemplos.oad;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.ejemplos.modelo.Actividad;
+
+/**
+ *
+ * @author JAguilar
+ */
+public class ActividadOad {
+
+    // Se utiliza un Map pues simula mejor la relacion de una entidad y su llave primaria 
+    // dentro de una base de datos relacional.    
+    private static Map<Integer, Actividad> actividades;
+    
+    static {
+        actividades = new HashMap<Integer,Actividad>();
+        actividades.put(1, new Actividad(1, "Reescribir capítulo 3", new Date(2011, 8, 30), 3));
+        actividades.put(2, new Actividad(2, "Resolver ejercicios de Estadística", new Date(2011, 9, 1), 1));
+    }
+    
+    public static List<Actividad> getActividades() {
+        return new ArrayList<Actividad>(actividades.values());
+    }
+}
