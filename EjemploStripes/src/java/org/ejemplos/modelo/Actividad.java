fea95675f46249fca9b23f3523052d9530b82b39
Julio Aguilar <madth3.dev@gmail.com>
2011-09-19 00:44:53 -0500

    Clases que representan los objetos del modelo de domino.

---
+++ EjemploStripes/src/java/org/ejemplos/modelo/Actividad.java
@@ -0,0 +1,79 @@
+package org.ejemplos.modelo;
+
+import java.util.Date;
+
+/**
+ *
+ * @author JAguilar
+ */
+public class Actividad {
+
+    private Integer id;
+    private String descripcion;
+    private Date plazoEntrega;
+    private Double avance;
+    private Integer idCategoria;
+
+    public Actividad(Integer id, String descripcion, Date plazoEntrega, Integer idCategoria) {
+        this.id = id;
+        this.descripcion = descripcion;
+        this.plazoEntrega = plazoEntrega;
+        this.idCategoria = idCategoria;
+    }
+
+    /**
+     * @return the id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the descripcion
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * @param descripcion the descripcion to set
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    /**
+     * @return the plazoEntrega
+     */
+    public Date getPlazoEntrega() {
+        return plazoEntrega;
+    }
+
+    /**
+     * @param plazoEntrega the plazoEntrega to set
+     */
+    public void setPlazoEntrega(Date plazoEntrega) {
+        this.plazoEntrega = plazoEntrega;
+    }
+
+    /**
+     * @return the avance
+     */
+    public Double getAvance() {
+        return avance;
+    }
+
+    /**
+     * @param avance the avance to set
+     */
+    public void setAvance(Double avance) {
+        this.avance = avance;
+    }
+}
